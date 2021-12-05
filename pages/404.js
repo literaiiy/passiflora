54f5834dd64cd82d8908f03b8605b4c3ec37a694
Ethan Yim <liddery69@gmail.com>
2021-12-05 14:51:01 -0800

    404 page, google analytics, css update

--- pages/404.js
+++ pages/404.js
@@ -2,15 +2,15 @@ import Nav from "../components/nav"
 
 export default function Custom404() {
   return (
-    <div>
+    <>
       <title>404 - Passiflora</title>
       <meta property="og:title" content={`404 - Passiflora`} />
       <Nav></Nav>
       <main id='error-main'>
-        <h1>404</h1>
+        <img id='passi-404' src="/images/passi-404.svg" alt="404" />
         <h2>An error has occured.</h2>
         <p>This page wasn't found.</p>
       </main>
-    </div>
+    </>
   )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta charset="UTF-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#bb4667" />
           <meta name="description" content="Passiflora is a customizable and minimalist time organization application used to easily manage schedules and routines for work, school, and much more." /> 
@@ -19,13 +20,22 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Crimson+Pro:wght@400;600&family=Noto+Serif+Display:ital,wght@1,500&display=swap" rel="stylesheet"/>
-          <link rel="stylesheet preload" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+          <link rel="stylesheet preload" as="style" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
 
           <meta property="og:site_name" content="Passiflora" />
           <meta property="og:url" content="https://passiflora.literaiiy.me" />
           <meta property="og:description" content="Passiflora is a customizable and minimalist time organization application used to easily manage schedules and routines for work, school, day-to-day activities, and much more." />
           <meta property="og:type" content="wesite" />
           <meta property="og:image" content="https://user-images.githubusercontent.com/64048778/144762674-247cbb50-e074-4df7-9648-a550d5120d1c.png" />
+
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+
+            gtag('config', 'G-8V90F1W1YG');
+          </script>
         </Head>
         <Main />
         <NextScript />
